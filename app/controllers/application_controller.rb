6084d24e65f16e584132bf185b6b2489650c0c50
Manuel Abaya <mjtabaya@gmail.com>
2018-09-18 12:46:54 +0800

    Add routes and Devise control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,37 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(*)
+    if current_user.admin?
+      admin_path
+    elsif current_user.bidder?
+      root_url
+    else
+      new_user_session_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def catch_not_found
+    yield
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_url, :flash => { :error => "Record not found." }
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:first_name, :last_name, :email,
+                         :password, :password_confirmation, { roles: [] })
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
+  get "auctions", to: "auctions#index"
+  post "bid", to: "bids#create"
+
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+
+  resources :users do
+    resources :auctions
+    resources :bids
+  end
+
+  resources :auctions do
+    resources :bid_entries
+  end
+
+  resources :bid_entries do
+    resources :products
+    resources :bids
+  end
 end
