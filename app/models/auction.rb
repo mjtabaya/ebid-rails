16bb69c4c6065e136c89085073fa4f1880ae0e53
Manuel Abaya <mjtabaya@gmail.com>
2018-09-18 13:36:14 +0800

    Add validation and status enum

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,2 +1,13 @@
 class Auction < ApplicationRecord
+  has_many :bid_entries, :dependent => :destroy
+  belongs_to :user, :dependent => :destroy
+  default_scope -> { order(date_auctioned: :desc) }
+  mount_uploader :hero_banner, PictureUploader
+  validates :user_id, :date_auctioned, :status, presence: true
+  enum status: %i[pending ongoing completed]
+  validates :status, inclusion: { in: status.keys }
+
+  def self.status_options
+    status.map { |k, _v| [k.humanize.capitalize, k] }
+  end
 end
